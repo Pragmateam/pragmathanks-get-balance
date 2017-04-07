d347d1ad272e30589ee42d0309e0a9392d276d85
Alabê Duarte <alabeduarte@gmail.com>
2017-04-07 15:59:23 +1000

    Introduce the very first iteratcion with google API

--- src/getBalance.js
+++ src/getBalance.js
@@ -1,3 +1,21 @@
-const getBalance = () => 'Your balance is 0 and you have 0 to give';
+const google = require('googleapis');
+const SPREADSHEET_ID = '';
+
+const getBalance = () => {
+  const auth = {};
+  const sheets = google.sheets('v4');
+
+  return new Promise((resolve, reject) => {
+    sheets.spreadsheets.values.get({
+      auth,
+      spreadsheetId: SPREADSHEET_ID,
+      range: 'Balance',
+    }, (err, response) => {
+      if (err) return reject(err);
+
+      return resolve('Your balance is 0 and you have 0 to give');
+    });
+  });
+};
 
 module.exports = getBalance;
--- test/getBalanceTest.js
+++ test/getBalanceTest.js
@@ -1,8 +1,51 @@
 const expect = require('chai').expect;
+const sinon = require('sinon');
+const google = require('googleapis');
+
 const getBalance = require('../src/getBalance');
 
 describe('getBalance', () => {
-  it('returns empty balance message', () => {
-    expect(getBalance('alabeduarte')).to.eql('Your balance is 0 and you have 0 to give');
+  sinon.stub(google, 'sheets').withArgs('v4');
+
+  const sheets = {
+    spreadsheets: {
+      values: {
+        get: (credentials, callback) => {
+          callback();
+        },
+      },
+    },
+  };
+
+  it('returns empty balance message', (done) => {
+    google.sheets.returns(sheets);
+
+    getBalance('alabeduarte').then((balance) => {
+      expect(balance).to.eql('Your balance is 0 and you have 0 to give');
+
+      done();
+    }).catch(err => done(err));
+  });
+
+  describe('when something goes wrong', () => {
+    const sheetsWithError = {
+      spreadsheets: {
+        values: {
+          get: (credentials, callback) => {
+            callback(new Error('Something went wrong'));
+          },
+        },
+      },
+    };
+
+    it('returns error', (done) => {
+      google.sheets.returns(sheetsWithError);
+
+      getBalance('alabeduarte').catch((err) => {
+        expect(err).to.eql(new Error('Something went wrong'));
+
+        done();
+      });
+    });
   });
 });
